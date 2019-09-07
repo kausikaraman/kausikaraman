129315efa6d358137da41980b8bde35862911a56
kausikaraman <54889722+kausikaraman@users.noreply.github.com>
2019-09-07 20:15:35 +0530

    Add files via upload

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie in 2009
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'blue'
+my_teeth = 'white'
+my_hair = 'brown'
+
+puts "lets talk about #{my_name}."
+puts "he's #{my_height} inches tall."
+puts "he's #{my_weight} pounds heavy."
+puts "actually that's not too heavy."
+puts "he's got #{my_eyes} eyes and #{my_hair} hair."
+puts "his teeth are usully #{my_teeth} depending on the coffee."
+
+#this line is tricky try to get it exactly righy
+puts "if i add #{my_age}, #{my_height}, and #{my_weight} i get #{my_age + my_height + my_weight}"
---
+++ ex9.rb
@@ -0,0 +1,14 @@
+# here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: #{days}"
+puts "here are the months: #{months}"
+
+puts %q{
+  There is something going on here.
+  With this wierd quote
+  We'll be able to type as much as we like.
+  Even 4 lines if we want, or 5, or 6.
+}
